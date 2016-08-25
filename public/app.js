26bcb83342edfd4422633d06b166f25990a49bcc
Alexandre Braga <alexandre_cgr@hotmail.com>
2016-08-25 12:20:48 -0400

    local library

--- public/app.js
+++ public/app.js
@@ -1,12 +1,55 @@
-var CommentBox = React.createClass({
+var Comment = React.createClass({
+    rawMarkup: function() {
+	var md = new Remarkable();
+	var rawMarkup = md.render(this.props.children.toString());
+	return { __html: rawMarkup };
+    },
+    render: function() {
+	
+	return (
+		<div className="comment">
+		<h2 className="commentAuthor">
+		{this.props.author}
+	    </h2>
+		<span dangerouslySetInnerHTML={this.rawMarkup()} />
+	    </div>
+	);
+    }
+});
+
+var CommentList = React.createClass({
     render: function() {
 	return (
-		<div className="commentBox">
-		Oi mundo! Isso aqui é um CommentBox.
+		<div className="commentList">
+		<Comment author="Pete Hunt">This is one comment</Comment>
+		<Comment author="Jordan Walke">This is *another* comment</Comment>
 		</div>
 	);
     }
 });
+
+var CommentForm = React.createClass({
+    render: function() {
+	return (
+		<div className="commentForm">
+		Oi! isso é um CommentForm.
+	    </div>
+	);
+    }
+});
+
+var CommentBox = React.createClass({
+    render: function() {
+	return (
+		<div className="commentBox">
+		<h1>Comentários</h1>
+		<CommentList />
+		<CommentForm />
+		</div>
+	);
+    }
+});
+
 
 ReactDOM.render(
 	<CommentBox/>,
